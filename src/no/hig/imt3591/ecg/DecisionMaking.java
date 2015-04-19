a7c0bdf85497ff8a2d10fc12a74e0a7f95f470c6
martinmine <martin_mine@hotmail.com>
2015-04-19 22:53:08 +0200

    Impl of trees started

--- src/no/hig/imt3591/ecg/DecisionMaking.java
+++ src/no/hig/imt3591/ecg/DecisionMaking.java
@@ -1,8 +1,27 @@
 package no.hig.imt3591.ecg;
 
+import no.hig.imt3591.id3.DecisionTree;
+
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by Martin on 17.04.2015.
  */
 public class DecisionMaking {
+    private static DecisionTree ecgTree;
+    private static DecisionTree pulseStressTree;
+    private static DecisionTree oxygenSaturationTree;
+    private static DecisionTree skinConductanceTree;
+
+    public static void initialize() {
+        // Train the trees
+    }
+
+    public static void onEnter(double ecg, double pulse, double pulseChange, double oxygen, double oxygenChange, double skinChange) {
+        double[] set = new double[] {ecg, pulse, pulseChange, oxygen, oxygenChange, skinChange};
+
+        //boolean ecgTriggered = ecgTree.search(set);
+    }
 
 }
